4bc300cf4cf2b20a1ee7436db46266057e27f49d
Kobina Abban-Saah <kabbansaah1@babson.edu>
2023-09-29 12:12:51 -0700

    till you drop

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>CIDR Block Party Calculator</div>
+        <div>CIDR Block Party till you drop Calculator</div>
       </header>
       <IPv4Addr />
       <footer>
