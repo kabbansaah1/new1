fd6e2131186a562fda724df484c70ba5f415e630
Kobina Abban-Saah <kabbansaah1@babson.edu>
2023-10-01 14:46:32 -0700

    8 with script

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>CIDR RAMBO7! Calculator</div>
+        <div>CIDR RAMBO8 with script! Calculator</div>
       </header>
       <IPv4Addr />
       <footer>
