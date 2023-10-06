b9b97ef77578ddc6dbd30faf2d86b77d7c0f9a0c
Kobina Abban-Saah <kabbansaah1@babson.edu>
2023-10-06 09:26:38 -0700

    MIC

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>CIDR Calculator testing</div>
+        <div>CIDR Calculator MIC</div>
       </header>
       <IPv4Addr />
       <footer>
